2e63c68faab95b296898638c5a81c4caaceb7a89
usrdevv <userdev.dc@gmail.com>
2025-10-05 19:01:59 +0200

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,1 +1,15 @@
+from colorama import Fore
+import random
 
+x = random.randit(1, 100)
+
+while True:
+    number = input("Entrer un nombre (1 - 100) : ")
+    
+    if number < x:
+        print("c'est +")
+    elif number > x:
+        print("c'est -")
+    else:
+        print("Vous avez Trouver le bon nombre")
+        print(Fore.GREEN + "Résultat correct" + Fore.WHITE)
