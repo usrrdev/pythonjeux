01883fca8a82d45f7592cc4a53c972120ceaa331
usrdevv <userdev.dc@gmail.com>
2025-10-05 18:57:37 +0200

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,15 +1,1 @@
-from colorama import Flore
-import random
 
-x = random.randit(1, 100)
-
-while True:
-    number = input("Entrer un nombre (1 - 100) : ")
-    
-    if number < x:
-        print("c'est +")
-    elif number > x:
-        print("c'est -")
-    else:
-        print("Vous avez Trouver le bon nombre")
-        print(Flore.GREEN + "Résultat correct" + Flore.WHITE)
